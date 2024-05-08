d2bd0f6ce1e76e0250c91d5e0dd783e44235cc41
Bogdan Putineanu <94795061+197Bogdan@users.noreply.github.com>
2024-05-08 23:37:18 +0300

    added run animations matched to movement

--- Assets/Code/Scripts/PlayerMovement/PlayerMovementController.cs
+++ Assets/Code/Scripts/PlayerMovement/PlayerMovementController.cs
@@ -8,38 +8,41 @@ public class PlayerMovementController : MonoBehaviour
     [SerializeField] private float speed = 0.25f;
 
     private PlayerInput playerInput;
-    private InputAction movementInput;
-    private string movementActionName = "Move";
+    private CharacterController characterController;
+    private Animator animator;
 
     private Vector2 inputVector;
     private Vector3 moveDirection;
 
-    private CharacterController characterController;
+    private InputAction movementInput;
+    private string movementActionName = "Move";
 
     void Start()
     {
         playerInput = GetComponent<PlayerInput>();
         movementInput = playerInput.actions.FindAction(movementActionName);
         characterController = GetComponent<CharacterController>();
+        animator = GetComponent<Animator>();
     }
 
     void FixedUpdate()
     {
-        RotatePlayer();
-        MovePlayer();
+        inputVector = GetInputVector();
+        AnimatePlayer(inputVector);
+        MovePlayer(inputVector);
+        RotatePlayerTowardsCursor();
     }
 
-    private void MovePlayer()
+    private void MovePlayer(Vector2 inputVector)
     {
-        inputVector = movementInput.ReadValue<Vector2>();
-        Vector3 direction = new Vector3(inputVector.x, 0, inputVector.y).normalized;
-        direction = Camera.main.transform.TransformDirection(direction);
-        direction.y = 0;
+        moveDirection = new Vector3(inputVector.x, 0, inputVector.y).normalized;
+        moveDirection = Camera.main.transform.TransformDirection(moveDirection);
+        moveDirection.y = 0;
 
-        characterController.Move(direction.normalized * speed * Time.fixedDeltaTime);
+        characterController.Move(moveDirection.normalized * speed * Time.fixedDeltaTime);
     }
 
-    private void RotatePlayer()
+    private void RotatePlayerTowardsCursor()
     {
         Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
         RaycastHit hit;
@@ -49,5 +52,22 @@ public class PlayerMovementController : MonoBehaviour
             transform.LookAt(hit.point);
             transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y, 0);
         }
+    }
+
+    private void AnimatePlayer(Vector2 inputVector)
+    {
+        // Rotate input vector based on camera rotation to move relative to camera, not world (e.g.: W moves player up the screen, not forward in world space)
+        // Rotate opposite to player to keep the input's direction independent of player's rotation (e.g.: player is facing to the right, pressing W should move the player up, not forward(right), so play MoveLeft animation to move up)
+        Vector3 rotatedInput = Quaternion.Euler(0, Camera.main.transform.eulerAngles.y - transform.eulerAngles.y, 0) * new Vector3(inputVector.x, 0, inputVector.y);
+        rotatedInput.Normalize();
+
+        animator.SetFloat("x", rotatedInput.x);
+        animator.SetFloat("y", rotatedInput.z); 
+        animator.SetBool("isMoving", inputVector.magnitude > 0);
+    }
+
+    private Vector2 GetInputVector()
+    {
+        return movementInput.ReadValue<Vector2>();
     }
 }
