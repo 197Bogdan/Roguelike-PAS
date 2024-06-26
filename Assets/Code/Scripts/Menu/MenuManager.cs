ea1aa15660170de2a9a39623cf0a7d8428e3ccbe
Bogdan Putineanu <94795061+197Bogdan@users.noreply.github.com>
2024-06-26 04:54:07 +0300

    fixed wrong resolution being set, added fps counter

--- Assets/Code/Scripts/Menu/MenuManager.cs
+++ Assets/Code/Scripts/Menu/MenuManager.cs
@@ -8,28 +8,41 @@ using UnityEngine.UI;
 public class MenuManager : MonoBehaviour
 {
 
+    List<Resolution> filteredResolutions;
     Resolution[] resolutions;
     public TMPro.TMP_Dropdown resolutionDropdown;
     int defaultResolutionIndex;
+    double defaultRefreshRate;
+
 
     void Start()
     {
+        defaultRefreshRate = Screen.currentResolution.refreshRateRatio.value;
         resolutions = Screen.resolutions;
         resolutionDropdown.ClearOptions();
 
-        List<string> options = new List<string>();
-        for(int i = 0; i < resolutions.Length; i++)
+        filteredResolutions = new List<Resolution>();
+        foreach(Resolution resolution in resolutions)
         {
-            string option = resolutions[i].width + " x " + resolutions[i].height;
+            if(resolution.refreshRateRatio.value == defaultRefreshRate)
+                filteredResolutions.Add(resolution);
+        }
+
+        List<string> options = new List<string>();
+        for(int i = 0; i < filteredResolutions.Count; i++)
+        {
+            string option = filteredResolutions[i].width + " x " + filteredResolutions[i].height;
             options.Add(option);
 
-            if(resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            if(filteredResolutions[i].width == Screen.currentResolution.width && filteredResolutions[i].height == Screen.currentResolution.height)
                 defaultResolutionIndex = i;
         }
 
         resolutionDropdown.AddOptions(options);
         resolutionDropdown.value = defaultResolutionIndex;
         resolutionDropdown.RefreshShownValue();
+
+        SetVSync(false);
     }
 
     public void StartGame()
@@ -49,7 +62,7 @@ public class MenuManager : MonoBehaviour
 
     public void SetResolution(int resolutionIndex)
     {
-        Resolution resolution = resolutions[resolutionIndex];
+        Resolution resolution = filteredResolutions[resolutionIndex];
         Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
         Debug.Log("Resolution: " + resolution.width + " x " + resolution.height);
     }
@@ -60,6 +73,6 @@ public class MenuManager : MonoBehaviour
             QualitySettings.vSyncCount = 1;
         else
             QualitySettings.vSyncCount = 0;
-        Debug.Log("VSync: " + QualitySettings.vSyncCount);
+        Debug.Log("VSync: " + isOn);
     }
 }
